b214e8b08b2e73f345b07b5b319040e0e9914681
Vedant Brahmbhatt <83278171+Vedantgitbot@users.noreply.github.com>
2025-06-22 16:44:16 -0500

    Update Chart.py

--- Chart.py
+++ Chart.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import plotly.express as px
+import numpy as np
 
 
 df = pd.read_csv('cleaned_data.csv')
@@ -11,7 +12,7 @@ df['listed_in'].fillna('Unknown', inplace=True)
 df['release_year'].fillna(0, inplace=True)
 
 
-def genre_distribution():
+def genre_distribution(df):
     genre_counts = df['listed_in'].str.split(', ').explode().value_counts().head(10)
     fig = px.bar(
         genre_counts,
@@ -20,70 +21,81 @@ def genre_distribution():
         labels={'x': 'Genre', 'y': 'Count'},
         title='Top 10 Genres Distribution',
         color=genre_counts.values,
-        color_continuous_scale='Blues'
+        color_continuous_scale='Reds'
     )
     return fig
 
-import plotly.express as px
+def movie_distribution_by_country(df):
+    import plotly.express as px
 
-def movies_by_country():
-    country_counts = df['country'].value_counts().reset_index()
-    country_counts.columns = ['country', 'count']
-    
-    color_scale = px.colors.sequential.Turbo
+    country_counts = df['country'].dropna().str.split(', ').explode().value_counts()
+    country_df = country_counts.reset_index()
+    country_df.columns = ['country', 'count']
 
     fig = px.choropleth(
-        country_counts,
+        country_df,
         locations='country',
         locationmode='country names',
         color='count',
-        color_continuous_scale=color_scale,
-        title='Movies Distribution by Country',
-        labels={'count': 'Number of Movies'},
-        template='plotly_dark'  
+        hover_name='country',
+        color_continuous_scale='Reds',
+        title='Movies/Shows Released by Country',
+        range_color=(0, country_df['count'].max())  
     )
-    
-    fig.update_layout(
-        coloraxis_colorbar=dict(
-            title="Number of Movies",
-            tickvals=[1, 10, 50, 100, 500, 1000, 2000],
-            ticktext=['1', '10', '50', '100', '500', '1000', '2000'],
-        ),
-        geo=dict(
-            showcoastlines=True,
-            coastlinecolor="Black",
-            showland=True,
-            landcolor="lightgray",
-            projection_type="natural earth"
-        )
-    )
-    
+
+    fig.update_layout(geo=dict(showframe=False, projection_type='natural earth'))
     return fig
 
 
-def release_year_trend():
+def release_year_trend(df):
     release_counts = df['release_year'].value_counts().sort_index()
+    
     fig = px.line(
-        release_counts,
         x=release_counts.index,
         y=release_counts.values,
-        labels={'x': 'Release Year', 'y': 'Number of Movies'},
-        title='Movie Release Trend Over the Years'
+        labels={'x': 'Release Year', 'y': 'Number of Releases'},
+        title='Release Trend Over the Years',
     )
+    
+   
+    fig.update_traces(line=dict(color='red'))
+    
     return fig
 
 
-def top_directors():
-    director_counts = df[df['director'] != 'Unknown']['director'].value_counts().head(10)
+def top_directors(df):
+    director_counts = (
+        df['director']
+        .dropna()
+        .str.split(', ')
+        .explode()
+    )
+
+    director_counts = director_counts[director_counts.str.lower() != 'unknown']
+
     
+    top_counts = director_counts.value_counts().head(10)
+
     fig = px.bar(
-        director_counts,
-        x=director_counts.index,
-        y=director_counts.values,
+        top_counts,
+        x=top_counts.index,
+        y=top_counts.values,
         labels={'x': 'Director', 'y': 'Count'},
-        title='Top 10 Directors with Most Movies',
-        color=director_counts.values,
-        color_continuous_scale='Viridis'
+        title='Top 10 Directors',
+        color=top_counts.index, 
+        color_discrete_sequence=['red']  
+    )
+    return fig
+
+def rating_distribution(df):
+    rating_counts = df['rating'].value_counts().nlargest(10)
+    fig = px.bar(
+        rating_counts, 
+        x=rating_counts.index, 
+        y=rating_counts.values,
+        labels={'x': 'Rating', 'y': 'Count'},
+        title='Content Rating Distribution',
+        color_discrete_sequence=['red']
     )
     return fig
 
